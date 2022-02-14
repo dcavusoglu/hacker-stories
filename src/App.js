31e7042ab001a9ec3c09f547824bbf847944cba0
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-14 16:06:32 +0300

    app js changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+const title = "React";
+const welcome = {
+  greeting: "Hello",
+  title: "React"
+}
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>{welcome.greeting} {getTitle('me')}!</h1>
+    <label htmlFor='search'>Search:</label>
+    <input id='search' type='text'/>
     </div>
   );
 }
