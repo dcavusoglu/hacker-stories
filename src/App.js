2eca1cdf4142fcfc1a0fb0756259b3df4b0d81e5
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-15 16:41:30 +0300

    react component and classes

--- src/App.js
+++ src/App.js
@@ -19,27 +19,58 @@ const list = [{
 }];
 
 
+class Developer {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getName() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const duygu = new Developer('Duygu', 'Cavusoglu');
+console.log(duygu.getName());
+
+const gizem = new Developer('Gizem', 'Cavusoglu');
+console.log(gizem.getName());
+
+
 function App() {
   return (
     <div>
       <h1>My hacker stories</h1>
       <label htmlFor='search'>Search:</label>
       <input id='search' type="text"/>
-
-    <hr/>
-    {list.map(item => {
-        return <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      })
-    }
+      <hr/>
+      <List/>
+       <hr/>
+      <List/>
     </div>
   );
 }
 
+function List() {
+
+    // list.map(item => {
+    //     return <div key={item.objectID}>
+    //       <span>
+    //         <a href={item.url}>{item.title}</a>
+    //       </span>
+    //       <span>{item.author}</span>
+    //       <span>{item.num_comments}</span>
+    //       <span>{item.points}</span>
+    //     </div>
+    //   })
+  const duygu = new Developer('Duygu', 'Cavusoglu');
+  const gizem = new Developer('Gizem', 'Cavusoglu');
+  return (
+    <div>
+      {duygu.getName()}
+      <br/>
+      {gizem.getName()}
+    </div>
+  )
+}
 export default App;
