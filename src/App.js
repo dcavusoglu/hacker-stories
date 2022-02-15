dfb71e47c15ebf61879e4d6a45ebd2952fe15cb4
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-15 20:58:17 +0300

    callback handlers

--- src/App.js
+++ src/App.js
@@ -1,54 +1,73 @@
+import React from 'react';
 import './App.css';
 
+// class Developer {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
 
-const list = [{
-  title: "React",
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walk',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-},
-{
-  title: "Redux",
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-}];
+//   getName() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+
+// const duygu = new Developer('Duygu', 'Cavusoglu');
+// console.log(duygu.getName());
+
+// const gizem = new Developer('Gizem', 'Cavusoglu');
+// console.log(gizem.getName());
 
 
-class Developer {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+const App = () => {
+  const stories = [{
+    title: "React",
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walk',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }];
+  const handleSearch = e => {
+    console.log(e.target.value);
   }
-
-  getName() {
-    return this.firstName + " " + this.lastName;
-  }
-}
-
-const duygu = new Developer('Duygu', 'Cavusoglu');
-console.log(duygu.getName());
-
-const gizem = new Developer('Gizem', 'Cavusoglu');
-console.log(gizem.getName());
-
-
-const App = () => (
+  return (
     <div>
       <h1>My hacker stories</h1>
-      <label htmlFor='search'>Search:</label>
-      <input id='search' type="text"/>
+      <Search onSearch={handleSearch}/>
       <hr/>
-      <List/>
+      <List list={stories}/>
     </div>
-)
+  );
+};
 
-const List = () => (
-    list.map(item => (
+const Search = props => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+    props.onSearch(e);
+  };
+  return (
+    <div>
+      <label htmlFor='search'>Search:</label>
+      <input id='search' type="text" onChange={handleChange}/>
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
+
+const List = props => (
+    props.list.map(item => (
         <div key={item.objectID}>
           <span>
             <a href={item.url}>{item.title}</a>
