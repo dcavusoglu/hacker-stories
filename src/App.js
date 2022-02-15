8209025b78a9ff0e781f6de16ed5a4270a1b22b7
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-02-15 10:03:29 +0300

    items mapped in the object

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import './App.css';
 
-const title = "React";
-const welcome = {
-  greeting: "Hello",
-  title: "React"
-}
-function getTitle(title) {
-  return title;
-}
+
+const list = [{
+  title: "React",
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walk',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+},
+{
+  title: "Redux",
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+}];
+
 
 function App() {
   return (
     <div>
-    <h1>{welcome.greeting} {getTitle('me')}!</h1>
-    <label htmlFor='search'>Search:</label>
-    <input id='search' type='text'/>
+      <h1>My hacker stories</h1>
+      <label htmlFor='search'>Search:</label>
+      <input id='search' type="text"/>
+
+    <hr/>
+    {list.map(item => {
+        return <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      })
+    }
     </div>
   );
 }
